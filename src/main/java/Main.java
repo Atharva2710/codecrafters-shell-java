b2991ae3dbcb2b988806298b5d2202a25cbdd5a4
Atharva Agrawal <atharva.25bcs10650@sst.scaler.com>
2026-06-16 18:56:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,8 +21,15 @@ public class Main {
                     }
                 }
                 System.exit(exitCode);
+            } else if (command.equals("echo")) {
+                if (input.startsWith("echo ")) {
+                    System.out.println(input.substring(5));
+                } else {
+                    System.out.println();
+                }
+            } else {
+                System.out.println(input + ": command not found");
             }
-            System.out.println(input + ": command not found");
         }
     }
 }
